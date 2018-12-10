ad56dcb70761551951c2e3540938afc14b0e2180
L-Yeiser <lauren.a.yeiser@gmail.com>
2018-12-10 15:20:06 -0800

    set up flow

---
+++ src/Landing/Landing.js
@@ -0,0 +1,16 @@
+// @flow
+
+import React, { PureComponent } from 'react';
+import { View, Text } from 'react-native';
+
+type Props = {};
+
+export default class Landing extends PureComponent<Props> {
+  render() {
+    return (
+      <View>
+        <Text>HELLO WORLD WORK WORK WORS</Text>
+      </View>
+    );
+  }
+}
